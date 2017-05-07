a184ac117e798a8dd081ee135ecf12cf02b79234
ethanryan <ethanryan@gmail.com>
2017-05-07 18:31:47 -0400

    database set up, models are good, starting to make controllers and pages in views

---
+++ html_twitter_app/app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+#   def new
+#     @user = User.new
+#   end
+#
+#   def create #got these from classmate
+#     if password_match?
+#       @user = User.new(user_params)
+#       @user.save
+#       session[:user_id] = @user.id
+#       redirect_to user_path(@user)
+#     else
+#       redirect_to new_user_path
+#     end
+#   end
+#
+#   def show
+#     @user = User.find(params[:id])
+#   end
+#
+#   private
+#
+#   def user_params
+#     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+#     #need to add authenticate above?
+#   end
+#
+#   def password_match?
+#     params[:user][:password] == params[:user][:password_confirmation]
+#   end
+#
+end
--- html_twitter_app/config/routes.rb
+++ html_twitter_app/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:index]
+  #get '/users', to 'users#index'
+  
 end
